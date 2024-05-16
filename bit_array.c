ca6a5e616baac400caa3b88c9b0c6e744e392e19
Max Kolenc <mkolenc@uvic.ca>
2024-05-15 19:35:24 -0700

    Added Null check for BitArray_free

--- bit_array.c
+++ bit_array.c
@@ -147,8 +147,10 @@ BitArray* BitArray_init_from_bin(const char* str)
 
 void BitArray_free(BitArray* bit_array)
 {
-    free(bit_array->data);
-    free(bit_array);
+    if (bit_array) {
+        free(bit_array->data);
+        free(bit_array);
+	}
 }
 
 BitArray* BitArray_resize(BitArray* bit_array, index_t size)
